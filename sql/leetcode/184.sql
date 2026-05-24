c3376c8d5addc26885ed70ec869c232eb775a266
Anton <ramtabog369@email.com>
2026-05-24 23:51:47 +0700

    feat: add SQL solutions LeetCode 181-184

---
+++ sql/leetcode/184.sql
@@ -0,0 +1,11 @@
+﻿-- LeetCode 184. Department Highest Salary
+-- ешение для ClickHouse
+SELECT d.Name AS Department, e.Name AS Employee, e.Salary
+FROM leetcode.Employee e
+JOIN leetcode.Department d ON e.DepartmentId = d.Id
+WHERE e.Salary = (
+    SELECT MAX(Salary)
+    FROM leetcode.Employee
+    WHERE DepartmentId = e.DepartmentId
+)
+ORDER BY d.Name;
