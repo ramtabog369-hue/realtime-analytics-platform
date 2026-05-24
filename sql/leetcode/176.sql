3648e2a0372b7bacc1d15e64ee259b24356bdb78
Anton <ramtabog369@email.com>
2026-05-24 19:57:24 +0700

    feat: add SQL solutions LeetCode 176, 177

---
+++ sql/leetcode/176.sql
@@ -0,0 +1,10 @@
+﻿-- LeetCode 176. Second Highest Salary
+-- ешение для ClickHouse
+SELECT 
+    IFNULL(
+        (SELECT DISTINCT Salary 
+         FROM leetcode.Employee 
+         ORDER BY Salary DESC 
+         LIMIT 1 OFFSET 1),
+        NULL
+    ) AS SecondHighestSalary;
---
+++ sql/leetcode/177.sql
@@ -0,0 +1,8 @@
+﻿-- LeetCode 177. Nth Highest Salary
+-- ример для N=3
+SELECT DISTINCT Salary
+FROM (
+    SELECT Salary, DENSE_RANK() OVER (ORDER BY Salary DESC) AS rnk
+    FROM leetcode.Employee
+) ranked
+WHERE rnk = 3;
