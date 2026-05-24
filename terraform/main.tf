955ce804b6057fcb12db62c71091976e6e42a6c4
Anton <ramtabog369@email.com>
2026-05-24 18:20:08 +0700

    feat: add Terraform config for existing Prometheus + Grafana

---
+++ terraform/main.tf
@@ -0,0 +1,32 @@
+﻿terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.12.1"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "prometheus"
+  namespace  = "monitoring"
+  create_namespace = true
+  timeout    = 300
+}
+
+resource "helm_release" "grafana" {
+  name       = "grafana"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "grafana"
+  namespace  = "monitoring"
+  create_namespace = true
+  timeout    = 300
+}
